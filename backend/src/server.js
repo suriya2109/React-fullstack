79a7f6b4575783d70eab2a5a543f5f1338bfd37f
Suriya <suriyacosmo@gmail.com>
2026-03-01 00:00:18 +0530

    The node js server has been connected.

---
+++ backend/src/server.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const app = express();
+
+app.use(express.json());
+
+let db;
+
+async function connectToDB() {
+    const uri = 'mongodb://127.0.0.1:27017';
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    await client.connect();
+
+    db = client.db('full-stack-react-db');
+}
+
+app.get('/api/articles/:name', async(req, res) => {
+    const { name } = req.params;
+    const article = await db.collection('articles').findOne({ name });
+    res.json(article);
+});
+
+app.post('/api/articles/:name/upvote', async(req, res) => {
+    const { name } = req.params;
+
+    const updatedArticle = await db.collection('articles').findOneAndUpdate({ name }, {
+        $inc: { upvotes: 1 }
+    }, {
+        returnDocument: "after",
+    });
+
+    res.json(updatedArticle);
+});
+
+app.post('/api/articles/:name/comments', async(req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    const newComment = { postedBy, text };
+
+    const updatedArticle = await db.collection('articles').findOneAndUpdate({ name }, {
+        $push: { comments: newComment }
+    }, {
+        returnDocument: 'after',
+    });
+
+    res.json(updatedArticle);
+});
+
+async function start() {
+    await connectToDB();
+    app.listen(8000, function() {
+        console.log('Server is listening on port 8000');
+    });
+}
+
+start();
