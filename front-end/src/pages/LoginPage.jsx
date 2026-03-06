3000aefe14da2244165e26463d8a5ca7b665a2aa
Suriya <suriyacosmo@gmail.com>
2026-03-06 22:53:14 +0530

    The intial form set up.

--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const LoginPage = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+
+  async function login(){
+    try{
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      navigate('/articles');
+    } catch(e){
+      setError(e.message);
+    }
+  }
+
   return (
-    <h1>Login Account</h1>
+    <>
+      <h1>Login Account</h1>
+
+      {error && <p>{error}</p>}
+
+      <input 
+        type="text" 
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      <input 
+        type="password" 
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+
+      <button onClick={login}>Login</button>
+
+      <Link to='/create-account'>
+        Don't have an account? Create one here
+      </Link>
+    </>
   )
 }
 
-export default LoginPage
+export default LoginPage;
